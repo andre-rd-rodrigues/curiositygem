6adc138750bf0093cb0b08776e564d10220f6e26
andrerodrigues <andrerodrigues29101992@gmail.com>
2023-02-02 23:43:41 +0000

    General clean up

--- assets/motion/motionVariants.js
+++ assets/motion/motionVariants.js
@@ -12,32 +12,6 @@ const containerVariant = {
   }
 };
 
-const containerDelayVariant = {
-  hidden: {
-    opacity: 0
-  },
-  visible: {
-    opacity: 1,
-    transition: {
-      delayChildren: 3,
-      staggerChildren: 0.5
-    }
-  }
-};
-
-//Blog
-const HomepageDelayVariant = {
-  hidden: {
-    opacity: 0
-  },
-  visible: {
-    opacity: 1,
-    transition: {
-      delayChildren: 0.5,
-      staggerChildren: 0.5
-    }
-  }
-};
 const fadeInVariant = {
   hidden: {
     opacity: 0
@@ -50,39 +24,4 @@ const fadeInVariant = {
   }
 };
 
-//Animated heading
-const wordTransition = [0.6, 0.02, 0.03, 0.9];
-
-const animatedHeadingChildren = {
-  hidden: {
-    opacity: 0,
-    y: "200%"
-  },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      type: "linear",
-      ease: wordTransition,
-      duration: 1.5
-    }
-  }
-};
-
-const animatedHeadingContainer = {
-  visible: {
-    transition: {
-      staggerChildren: 0.03
-    }
-  }
-};
-
-export {
-  containerVariant,
-  containerDelayVariant,
-  animatedHeadingChildren,
-  animatedHeadingContainer,
-  fadeInVariant,
-  HomepageDelayVariant,
-  motion
-};
+export { containerVariant, fadeInVariant, motion };
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import "styles/global.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.js";
 
 export default function App({ Component, pageProps }) {
   return (
