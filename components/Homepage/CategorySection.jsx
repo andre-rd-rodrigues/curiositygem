d4b3d66cd4fb494848dcb2e5080dd22fb561cda8
andrerodrigues <andrerodrigues29101992@gmail.com>
2023-02-12 22:10:45 +0000

    Add results page

--- components/Homepage/CategorySection.jsx
+++ components/Homepage/CategorySection.jsx
@@ -10,6 +10,7 @@ const CategorySection = ({ category }) => {
 
   // Create title according to category type
   const title = CATEGORIES.find(({ type }) => type === category).name;
+
   const articles = getArticlesByCategory(category);
 
   const articles = getArticlesByCategory(category);
@@ -29,7 +30,7 @@ const CategorySection = ({ category }) => {
 };
 
 CategorySection.propTypes = {
-  categoryType: PropTypes.string
+  categoryType: PropTypes.string.isRequired
 };
 
 export default CategorySection;
