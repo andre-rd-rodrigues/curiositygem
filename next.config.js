3b85ee9951645a047de505cfc359e1d80cf650ce
andrerodrigues <andrerodrigues29101992@gmail.com>
2023-03-07 14:57:22 +0000

    Add google analytics

--- next.config.js
+++ next.config.js
@@ -4,5 +4,11 @@ const path = require("path");
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")]
+  },
+
+  // for further context: https://www.jussivirtanen.fi/writing/handling-api-rate-limits-with-nextjs
+  experimental: {
+    workerThreads: false,
+    cpus: 1
   }
 };
--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,28 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ArticlesProvider from "context/articles-context";
 import "styles/global.scss";
 import Layout from "../components/Layout/Layout";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <ArticlesProvider>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-XTDYFCW8JD"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-XTDYFCW8JD', {page_path: window.location.pathname,
+});
+`
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
