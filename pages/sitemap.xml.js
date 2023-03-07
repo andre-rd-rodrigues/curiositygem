b6969fbf5ca98eba6128d6d9359f33bc6d06bb46
andrerodrigues <andrerodrigues29101992@gmail.com>
2023-03-07 17:40:31 +0000

    Fix sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,21 +1,13 @@
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
      ${posts
-       .map(({ slug }) => {
-         return `
+       .map(
+         ({ slug }) => `
        <url>
-           <loc>${`${EXTERNAL_DATA_URL}/${slug}`}</loc>
-       </url>
-     `;
-       })
+           <loc>${`https://curiositygem.com/article/${slug}`}</loc>
+       </url>`
+       )
        .join("")}
    </urlset>
  `;
